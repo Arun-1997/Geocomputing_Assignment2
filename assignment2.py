ca1d85056be2ce5e4c40064245e3fbb136bfa36a
AArora18 <annaarora18@gmail.com>
2021-12-16 16:41:07 +0100

    Task 2 complete

--- assignment2.py
+++ assignment2.py
@@ -117,28 +117,45 @@ print(json.dumps(raster_statistics,indent=2))
 
 ############# 2 ################
 print("#" * 10, " Block 2", "#" * 10)
-print()
 
 catchment_data_file = "catchments.shp"
 city_catchmentsDs = ogr.Open(catchment_data_file)
 
-input_layer = city_catchmentsDs.GetLayer(2)
+#Extracting the shapefile layer
+input_layer = city_catchmentsDs.GetLayer(0)
 
-print("x_min = %.2f x_max = %.2f y_min = %.2f y_max = %.2f" % ())
+#2.1
+layerExtents = input_layer.GetExtent()      #Getting the layer extents
+print("x_min = %.2f x_max = %.2f y_min = %.2f y_max = %.2f" % (layerExtents[0],
+layerExtents[1], layerExtents[2], layerExtents[3]))
 
-layerDefinition = input_layer.GetLayerDefn()
+#2.2
+layerDefinition = input_layer.GetLayerDefn()    #Getting the layer definition for further metadata
+fieldCount = layerDefinition.GetFieldCount()
 print("Number of fields: " + str(fieldCount))
 
-fields = []
-
+#Printing the field name
+fields = [layerDefinition.GetFieldDefn(i).GetName() for i in range(fieldCount)]
 print("Fields: ", fields)
 
+#2.3
+layerFeatureNum = input_layer.GetFeatureCount()     #Getting the number of features
 print("Number of features: " + str(layerFeatureNum))
 
+#2.4
+
+#Creating a dictionary of areas and object id of all the catchment
+areas = {}
+for feature in input_layer:
+    featureArea = feature.GetGeometryRef().Area()
+    areas[int(feature['objectid'])] = featureArea
+
+#Extracting the minimum area and the corresponding object id
 small_and_largeCatchments = {
-    "small": {"area": 0, "objectid": ""},
-    "large": {"area": 0, "objectid": ""},
+    "small": {"area": min(areas.values()), "objectid": str(min(areas, key=areas.get))},
+    "large": {"area": max(areas.values()), "objectid": str(max(areas, key=areas.get))},
 }
+
 
 print(
     "largest catchment: ",
@@ -154,101 +171,100 @@ print(
     small_and_largeCatchments["small"]["area"],
 )
 
-print()
 
-############# 3 ################
-
-print("#" * 20, " Block 3", "#" * 20)
-print()
-
-most_important_crop_per_catchment = {}
-for feature in input_layer:
-    sdn_crops_clippedDs = gdal.Warp(
-        cutlineDSName=catchment_data_file,
-        cutlineWhere="objectid = {}".format(objectId),
-        cropToCutline=False,
-    )
-
-    sdn_crops_prodArray = gdarr.DatasetReadAsArray(input_layer)
-    sdn_crops_prodArray[sdn_crops_prodArray > no_data_value] = 0
-
-    most_important_crop_per_catchment[objectId] = [
-        most_important_crop,
-        int(most_important_crop_amount),
-        0,
-    ]
-
-
-############# 4 ################
-print("#" * 10, " Block 4", "#" * 10)
-print()
-
-
-cred_path = r"..\credentials.json"
-
-# Read the login details from json file for safe connection.
-with open(cred_path, "r") as login:
-    db_con_data = json.load(login)
-
-db_host = db_con_data["host"]
-
-conn_string = "host='%s' port='%d' user='%s' password='%s' dbname='%s'" % (
-    db_host,
-    db_port,
-    db_username,
-    db_pwd,
-    db_name,
-)
-
-with psycopg2.connect(conn_string) as conn:
-    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-        selectQuery = """
-        """
-        cursor.execute(selectQuery)
-        records = cursor.fetchall()
-        for record in records:
-            most_important_crop_per_catchment[record["objectid"]][2] = int(
-                sum(crops_cons_total)
-            )
-
-
-print(most_important_crop_per_catchment)
-print()
-
-############# 5 ################
-print("#" * 10, " Block 5", "#" * 10)
-print()
-
-output_data_file = "sdn_market_dynamics.json"
-driver = ogr.GetDriverByName("GeoJSON")
-output_ds = driver.CreateDataSource(output_data_file)
-
-field_names = ["crop", "prod", "cons"]
-
-for infeature in output_layer:
-    objectId = infeature.GetFieldAsInteger("objectid")
-    if objectId in most_important_crop_per_catchment.keys():
-        outfeature.SetField(
-            field_names[0], most_important_crop_per_catchment[objectId][0]
-        )
-        outfeature.SetGeometry(outfeature.GetGeometryRef())
-        output_layer.AddFeature(outfeature)
-
-input_layer = None
-output_layer = None
-city_catchmentsDs = None
-output_ds = None
-
-############# 6 ################
-print("#" * 10, " Block 6", "#" * 10)
-print()
-
-speedups.disable()
-
-print(catchment_df)
-print()
-
-catchment_df.plot(column="diff", legend="True")
-plt.show()
-
-############# END ################
+# ############# 3 ################
+#
+# print("#" * 20, " Block 3", "#" * 20)
+# print()
+#
+# most_important_crop_per_catchment = {}
+# for feature in input_layer:
+#     sdn_crops_clippedDs = gdal.Warp(
+#         cutlineDSName=catchment_data_file,
+#         cutlineWhere="objectid = {}".format(objectId),
+#         cropToCutline=False,
+#     )
+#
+#     sdn_crops_prodArray = gdarr.DatasetReadAsArray(input_layer)
+#     sdn_crops_prodArray[sdn_crops_prodArray > no_data_value] = 0
+#
+#     most_important_crop_per_catchment[objectId] = [
+#         most_important_crop,
+#         int(most_important_crop_amount),
+#         0,
+#     ]
+#
+#
+# ############# 4 ################
+# print("#" * 10, " Block 4", "#" * 10)
+# print()
+#
+#
+# cred_path = r"..\credentials.json"
+#
+# # Read the login details from json file for safe connection.
+# with open(cred_path, "r") as login:
+#     db_con_data = json.load(login)
+#
+# db_host = db_con_data["host"]
+#
+# conn_string = "host='%s' port='%d' user='%s' password='%s' dbname='%s'" % (
+#     db_host,
+#     db_port,
+#     db_username,
+#     db_pwd,
+#     db_name,
+# )
+#
+# with psycopg2.connect(conn_string) as conn:
+#     with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+#         selectQuery = """
+#         """
+#         cursor.execute(selectQuery)
+#         records = cursor.fetchall()
+#         for record in records:
+#             most_important_crop_per_catchment[record["objectid"]][2] = int(
+#                 sum(crops_cons_total)
+#             )
+#
+#
+# print(most_important_crop_per_catchment)
+# print()
+#
+# ############# 5 ################
+# print("#" * 10, " Block 5", "#" * 10)
+# print()
+#
+# output_data_file = "sdn_market_dynamics.json"
+# driver = ogr.GetDriverByName("GeoJSON")
+# output_ds = driver.CreateDataSource(output_data_file)
+#
+# field_names = ["crop", "prod", "cons"]
+#
+# for infeature in output_layer:
+#     objectId = infeature.GetFieldAsInteger("objectid")
+#     if objectId in most_important_crop_per_catchment.keys():
+#         outfeature.SetField(
+#             field_names[0], most_important_crop_per_catchment[objectId][0]
+#         )
+#         outfeature.SetGeometry(outfeature.GetGeometryRef())
+#         output_layer.AddFeature(outfeature)
+#
+# input_layer = None
+# output_layer = None
+# city_catchmentsDs = None
+# output_ds = None
+#
+# ############# 6 ################
+# print("#" * 10, " Block 6", "#" * 10)
+# print()
+#
+# speedups.disable()
+#
+# print(catchment_df)
+# print()
+#
+# catchment_df.plot(column="diff", legend="True")
+# plt.show()
+#
+# ############# END ################
